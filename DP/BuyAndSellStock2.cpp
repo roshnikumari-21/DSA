48c4746ba82bd0e8d8c9a9130c56088d9c72c44d
Roshni Kumari <roshnikumari212004@gmail.com>
2026-05-28 14:45:42 +0530

    update 16

--- DP/BuyAndSellStock2.cpp
+++ DP/BuyAndSellStock2.cpp
@@ -69,3 +69,23 @@ public:
         return dp[0][1];
     }
 };
+
+
+//one more method_____without dp_______________________
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int ans = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (prices[i] < prices[i + 1]) {
+                ans += (prices[i + 1] - prices[i]);
+            }
+        }
+
+        return ans;
+    }
+};
